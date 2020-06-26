ee09c2f1a225ff43561a16602a1104bf9c1fcabc
Aaron Ong <aaon@splitmedialabs.com>
2020-06-26 02:36:21 -0700

    Initial React app setup

---
+++ docs/precache-manifest.f97255f70dd007ad5a3f098b776310fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972b0686afaf2c6c0cfbe47594d3da70",
+    "url": "/wall-detecting-robot/index.html"
+  },
+  {
+    "revision": "166a4036a0ad82d36bee",
+    "url": "/wall-detecting-robot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da5354a9f51f89341f4e",
+    "url": "/wall-detecting-robot/static/js/2.039fa7f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wall-detecting-robot/static/js/2.039fa7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166a4036a0ad82d36bee",
+    "url": "/wall-detecting-robot/static/js/main.1170e611.chunk.js"
+  },
+  {
+    "revision": "1ae9d6372f9bc9654bd8",
+    "url": "/wall-detecting-robot/static/js/runtime-main.1822cd1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wall-detecting-robot/static/media/logo.5d5d9eef.svg"
+  }
+]);
